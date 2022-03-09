ca989d98b1f972c759e39e6849fe26ec49b2eb6f
Vicente Garcia <vicen1@hotmail.com>
2022-03-09 17:07:21 -0600

    Add functionality to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,8 @@
 var currentDay = $("#currentDay");
 var timeBlocksArea = $(".container");
 var updateStatus;
-var iniWorkHour = 0;
-var lastWorkHour = 23;
-var getCurrentDay = function(show){
-    var inFormat = "dddd, MMMM Do YYYY, h:mm:ss a";
-    currentDay.text(moment().format(inFormat));
-    if (show === "complete"){
-        setInterval(function(){
-            currentDay.text(moment().format(inFormat));
-        }, 1000);
-    }else{
-        inFormat = "L";
-        var date = moment().format(inFormat); 
-        console.log(date);
-    };
-};
+var iniWorkHour = 7;
+var lastWorkHour = 20;
 var setBlockStatus = function(){
     $(".time-blocks textarea").each(function(index, block){
         var hourBlock = parseInt(block.id);
@@ -46,14 +33,69 @@ var getTimeBlocks = function(){
         }else if (i > 12){
             hour = i - 12;
         };
-        timeBlock.append("<p class='hour'>" + hour + " " + meridiem + "</p><textarea id=" + i + "></textarea><button class='saveBtn'></button>");
+        timeBlock.append("<p class='hour'>" + hour + " " + meridiem + "</p><textarea id=" + i + "></textarea><button id='btn-" + i + "'class='saveBtn'><i class='fa fa-save'></i></button>");
         timeBlocksArea.append(timeBlock);
     };
     setBlockStatus();
-    setInterval(function(){
-        setBlockStatus();
-    }, 10000);
 };
-getCurrentDay("complete");
-getTimeBlocks();
+var getCurrentDay = function(show){
+    var inFormat = "dddd, MMMM Do YYYY, h:mm:ss a";
+    var lastTime = moment().format("HH");
+    currentDay.text(moment().format(inFormat));
+    if (show === "show"){
+        setInterval(function(){
+            currentDay.text(moment().format(inFormat));
+            if (lastTime < moment().format("HH")){
+                setBlockStatus();
+            };
+        }, 1000);
+        getTimeBlocks();
+    }else{
+        inFormat = "L";
+        var date = moment().format(inFormat); 
+        console.log(date);
+    };
+};
 
+getCurrentDay("show");
+$(".time-blocks").on("change", "textarea", function(){
+    var numId = this.id;
+    var blockChange = ".time-blocks #btn-" + numId;
+    $(blockChange).addClass("btnUpdate");
+});
+$(".time-blocks").on("click", "button", function(){
+    $(this).removeClass("btnUpdate");
+});
+$(".time-blocks").on("focus", "textarea", function(){
+    $(this).addClass("focusUpdate");
+});
+$(".time-blocks").on("focusout", "textarea", function(){
+    $(this).removeClass("focusUpdate");
+});
+$(".time-blocks textarea").mouseover(function(){
+    $(this).addClass("onUpdate");
+});
+$(".time-blocks textarea").mouseleave(function(){
+    $(this).removeClass("onUpdate");
+});
+
+// save button in modal was clicked
+//$(".time-blocks .saveBtn").click(function() {
+    //var prueba = $(this);
+    //console.log(prueba);
+    // get form values
+    //var taskText = $("#modalTaskDescription").val();
+  
+    /*if (taskText && taskDate) {
+      createTask(taskText, taskDate, "toDo");
+  
+      // close modal
+      $("#task-form-modal").modal("hide");
+  
+      // save in tasks array
+      tasks.toDo.push({
+        text: taskText,
+        date: taskDate
+      });
+    }*/
+  //});
